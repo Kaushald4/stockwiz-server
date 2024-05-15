3ed9724097e072fa5c052ac8ba506cceff227b7a
Kaushal Mehta <kaushalkumar304@gmail.com>
2024-05-15 06:06:54 +0530

    adding cors origin

--- stockwiz/settings.py
+++ stockwiz/settings.py
@@ -137,7 +137,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 
 CORS_ORIGIN_WHITELIST = (
   'http://localhost:3000',
-  "https://stcokwiz-assignment.vercel.app/login"
+  "https://stcokwiz-assignment.vercel.app"
 )
 
 # Internationalization
